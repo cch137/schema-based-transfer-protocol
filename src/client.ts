7ed7dbd3a79b9caad0e1dce824ec2d40178087e0
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-05 23:46:53 +0800

    restruct

--- src/client.ts
+++ src/client.ts
@@ -1,49 +1,47 @@
-const unpackData = (array: Uint8Array) =>
-  JSON.parse(new TextDecoder().decode(array.reverse().map((v) => ~v & 0xff)));
+(() => {
+  const unpackData = (array: Uint8Array) =>
+    JSON.parse(new TextDecoder().decode(array.reverse().map((v) => ~v & 0xff)));
 
-const packData = <T = any>(data: T) => {
-  return new TextEncoder()
-    .encode(JSON.stringify(data))
-    .map((v) => ~v & 0xff)
-    .reverse();
-};
+  const packData = <T = any>(data: T) => {
+    return new TextEncoder()
+      .encode(JSON.stringify(data))
+      .map((v) => ~v & 0xff)
+      .reverse();
+  };
 
-class Tracker {
-  static readonly UID_KEY = "t";
-  static tracker?: Tracker;
-  static isBlur = false;
-  static currentHref = "";
+  const record = (type: string, data: Record<string, any> = {}) => {
+    if (ws.readyState !== ws.OPEN) {
+      const sending = () => {
+        record(type, data);
+        ws.removeEventListener("open", sending);
+      };
+      ws[addEventListener]("open", sending);
+      return;
+    }
+    ws.send(packData({ ...data, type }));
+  };
 
-  static init() {
-    window.addEventListener("blur", () => {
-      Tracker.isBlur = true;
-      Tracker.tracker!.record("blur");
-    });
-    window.addEventListener("focus", () => {
-      Tracker.isBlur = false;
-      Tracker.tracker!.record("focus");
-    });
-    return Tracker.tracker || (Tracker.tracker = new Tracker());
-  }
+  const recordView = () => {
+    const href = location.href;
+    currHref = href;
+    record("view", { href });
+  };
 
-  readonly ws: WebSocket;
+  const addEventListener = "addEventListener";
+  const UID_KEY = "t";
+  const getUid = () => localStorage.getItem(UID_KEY) || "";
+  const setUid = (v: string) => localStorage.setItem(UID_KEY, v);
 
-  get uid() {
-    return localStorage.getItem(Tracker.UID_KEY) || "";
-  }
-  set uid(v: string) {
-    localStorage.setItem(Tracker.UID_KEY, v);
-  }
+  let currHref = "";
+  let ws: WebSocket;
 
-  constructor() {
-    Tracker.tracker = this;
-    const ws = new WebSocket(`wss://space.cch137.link/${this.uid || ""}`);
+  const createTracker = () => {
+    ws = new WebSocket(`wss://space.cch137.link/${getUid()}`);
     // const ws = new WebSocket(`ws://localhost:4000/${this.uid}`);
-    this.ws = ws;
 
     let hbItv: NodeJS.Timeout;
 
-    ws.addEventListener("message", async (ev) => {
+    ws[addEventListener]("message", async (ev) => {
       // parse command pack from server
       const { cmd, ...data } = unpackData(
         new Uint8Array(await (ev.data as Blob).arrayBuffer())
@@ -51,54 +49,38 @@ class Tracker {
       // execute command
       switch (cmd) {
         case "uid": {
-          this.uid = data.uid;
+          setUid(data.uid);
           clearInterval(hbItv);
           hbItv = setInterval(() => {
             if (ws.readyState !== ws.OPEN) return clearInterval(hbItv);
-            if (Tracker.currentHref !== location.href) this.recordView();
+            if (currHref !== location.href) recordView();
             else ws.send(new Uint8Array([0]));
           }, 1000);
           break;
         }
         case "view": {
-          this.recordView();
+          recordView();
           break;
         }
       }
     });
 
-    ws.addEventListener("error", (ev) => {
+    ws[addEventListener]("error", (ev) => {
       console.error(ev);
       ws.close();
     });
 
-    ws.addEventListener("close", () => {
+    ws[addEventListener]("close", () => {
       const itv = setInterval(() => {
-        if (Tracker.isBlur) return;
+        if (document.hasFocus()) return;
         clearInterval(itv);
-        Tracker.tracker = new Tracker();
+        createTracker();
       }, 1000);
     });
-  }
+  };
 
-  record(type: string, data: Record<string, any> = {}) {
-    const { ws } = Tracker.tracker!;
-    if (ws.readyState !== ws.OPEN) {
-      const sending = () => {
-        Tracker.tracker!.record(type, data);
-        ws.removeEventListener("open", sending);
-      };
-      ws.addEventListener("open", sending);
-      return;
-    }
-    ws.send(packData({ ...data, type }));
-  }
+  window[addEventListener]("blur", () => record("blur"));
+  window[addEventListener]("focus", () => record("focus"));
 
-  recordView() {
-    const href = location.href;
-    Tracker.currentHref = href;
-    Tracker.tracker!.record("view", { href });
-  }
-}
-
-Tracker.init();
+  createTracker();
+})();
