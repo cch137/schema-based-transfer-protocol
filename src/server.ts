bb7d5f3109cf47f486924381f58f91ef53fa38b2
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-06 23:51:27 +0800

    fix: arrange code

--- src/server.ts
+++ src/server.ts
@@ -6,60 +6,18 @@ import Logger from "./lib/logger";
 
 env();
 
-const unpackData = (array: Buffer) =>
-  JSON.parse(new TextDecoder().decode(array.reverse().map((v) => ~v & 0xff)));
-
-export const packData = <T = any>(data: T) =>
-  typeof Buffer === "undefined"
-    ? new TextEncoder()
-        .encode(JSON.stringify(data))
-        .map((v) => ~v & 0xff)
-        .reverse()
-    : Buffer.from(JSON.stringify(data))
-        .map((v) => ~v & 0xff)
-        .reverse();
-
 const V_TAG = "1";
 const UID_LENGTH = 16;
 const SID_LENGTH = 16;
+const PORT = Number(process.env.PORT) || 4000;
 
 mongoose
   .connect(process.env.MONGODB_URI!)
   .then(() => console.log("connected to MongoDB"))
   .catch(() => console.error("failed to connect to MongoDB"));
 
-const db = {
-  collection: (name: string) => mongoose.connection.collection(name),
-  get users() {
-    return db.collection("users");
-  },
-  get tracks() {
-    return db.collection("tracks");
-  },
-};
-
-function generate64BitId(length: number): string {
-  const charset: string =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
-  const charsetLength: number = charset.length;
-  let result: string = "";
-  for (let i: number = 0; i < length; i++) {
-    const randomIndex: number = Math.floor(Math.random() * charsetLength);
-    result += charset[randomIndex];
-  }
-  return result;
-}
-
-async function uidIsExist(uid: string) {
-  return Boolean(uid) && Boolean(await db.users.findOne({ uid }));
-}
-
-async function generateUser() {
-  const uid = generate64BitId(UID_LENGTH);
-  if (await uidIsExist(uid)) return generateUser();
-  return await User.create({ uid, ip: [], ua: [] });
-}
-
+const Users = mongoose.connection.collection("users");
+const Tracks = mongoose.connection.collection("tracks");
 const User = mongoose.model(
   "User",
   new mongoose.Schema(
@@ -83,28 +41,58 @@ const User = mongoose.model(
   { overwriteModels: true }
 );
 
+const generate64BitId = (length: number): string => {
+  const charset: string =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
+  const charsetLength: number = charset.length;
+  let result: string = "";
+  for (let i: number = 0; i < length; i++) {
+    const randomIndex: number = Math.floor(Math.random() * charsetLength);
+    result += charset[randomIndex];
+  }
+  return result;
+};
+
+async function uidIsExist(uid: string) {
+  return Boolean(uid) && Boolean(await Users.findOne({ uid }));
+}
+
+async function generateUser() {
+  const uid = generate64BitId(UID_LENGTH);
+  if (await uidIsExist(uid)) return generateUser();
+  return await User.create({ uid, ip: [], ua: [] });
+}
+
+const unpackData = (array: Buffer) =>
+  JSON.parse(new TextDecoder().decode(array.reverse().map((v) => ~v & 0xff)));
+
+const packData = <T = any>(data: T) =>
+  typeof Buffer === "undefined"
+    ? new TextEncoder()
+        .encode(JSON.stringify(data))
+        .map((v) => ~v & 0xff)
+        .reverse()
+    : Buffer.from(JSON.stringify(data))
+        .map((v) => ~v & 0xff)
+        .reverse();
+
 const packCommand = (cmd: string, data: { [key: string]: any } = {}) =>
   packData({ cmd, ...data });
-
-const addTrack = (
-  uid: string,
-  sid: string,
-  type: string = "unknown",
-  data: { [key: string]: any } = {}
-) => {
-  delete data.uid;
-  delete data.sid;
-  delete data.type;
-  delete data.t;
-  db.tracks.insertOne({ uid, sid, type, t: new Date(), ...data });
-};
 
 const server = net.createServer((socket) => {
   let uid = "";
   const sid = generate64BitId(SID_LENGTH);
 
-  const record = (type?: string, data?: { [key: string]: any }) =>
-    addTrack(uid, sid, type, data);
+  const record = (
+    type: string = "unknown",
+    data: { [key: string]: any } = {}
+  ) => {
+    delete data.uid;
+    delete data.sid;
+    delete data.type;
+    delete data.t;
+    Tracks.insertOne({ uid, sid, type, t: new Date(), ...data });
+  };
 
   socket.on("upgrade", async ({ headers, body }) => {
     const [_, vTag, _uid] = (headers[":path:"] || "").split("/");
@@ -151,12 +139,11 @@ const server = net.createServer((socket) => {
       record("disconn");
       Logger.info(`[${uid}] disconnected`);
     } else {
-      db.tracks.deleteMany({ sid });
+      Tracks.deleteMany({ sid });
     }
   });
 });
 
-const PORT = Number(process.env.PORT) || 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
